a71a6dad4b031d0166693769f9bb9feda9278ca0
sendo-kakeru <sendokakeru.js@gmail.com>
2025-03-21 18:00:48 +0900

    feat(base): create base

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 
+fn router() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Home" }))
+        .route("/hello", get(|| async { "Hello World!" }))
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello World!" }));
+    let app = router();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
